f70e9fa439d4d2ac32a6ea280d6adc763802f4f4
larry-main <laraotwell@gmail.com>
2023-10-11 19:12:29 +0800

    feat:  azure stt timeout test

--- src/components/AzureSpeechToText.tsx
+++ src/components/AzureSpeechToText.tsx
@@ -97,7 +97,7 @@ const AzureSpeechToText: React.FC<AzureSpeechToTextProps> = ({
       setIsListening(false);
       setWaiting(false);
       console.log('Speech recognition stopped due to timeout');
-    }, 2 * 60 * 1000);
+    }, 5  * 1000);
 
     newRecognizer.startContinuousRecognitionAsync(
       () => {
